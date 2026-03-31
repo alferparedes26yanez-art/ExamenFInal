2609c3279a1fef268964be46d8e93e332f68ee5e
alferparedes26yanez-art <alferparedes26yanez@gmail.com>
2026-03-30 20:52:00 -0500

    reaion erenia ave

---
+++ src/Gato.java
@@ -0,0 +1,14 @@
+//creamos la herencia
+public class Gato extends Mascota {
+
+    
+    public Gato(int id, String nombre, int edad) {
+        super(id, nombre, edad);
+    }
+
+
+    @Override
+    public String hacerSonido() {
+        return "Miaw Miaw";
+    }
+}
